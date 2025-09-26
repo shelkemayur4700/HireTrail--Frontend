502e112470c1a80b499b2255ad3b09273448ba14
Mayurs0 <mayurs@iconnectsolutions.com>
2025-09-26 12:35:50 +0530

    [FEAT]:shadcn compo added

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
-const App = () => {
-  return <div>App</div>;
+import type { FC } from "react";
+import { Button } from "./components/ui/button";
+
+const App: FC = () => {
+  return (
+    <div className="h-screen flex items-center  justify-center">
+      <h2>App</h2>
+      <Button variant={"ghost"}>Hello</Button>
+    </div>
+  );
 };
 
 export default App;
